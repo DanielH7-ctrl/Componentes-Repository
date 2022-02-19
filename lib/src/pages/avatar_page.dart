5fafbf50beaa45bb69eb6c23f16cf235b548453b
DanielH7-ctrl <danielhuerta826@gmail.com>
2022-02-19 00:43:45 -0500

    Circle Avatar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -10,6 +10,31 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://www.cinemascomics.com/wp-content/uploads/2021/05/Asi-nacio-la-idea-de-Spider-Man-contado-por-Stan-Lee.jpg'),
+              radius: 20.0,
+            ),
+          ),
+
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.green,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage('https://img.ecartelera.com/noticias/fotos/50500/50500/1.jpg'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
       ),
     );
   }
