708540fa4c3fb17ef756b056f9e44b7477663dc3
DanielH7-ctrl <danielhuerta826@gmail.com>
2022-02-19 02:20:18 -0500

    Input Fields

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,5 +1,6 @@
 import 'package:componentes/src/pages/animated_container.dart';
 import 'package:componentes/src/pages/card_page.dart';
+import 'package:componentes/src/pages/input_page.dart';
 import 'package:flutter/material.dart';
 
 import '../pages/alert_page.dart';
@@ -14,6 +15,7 @@ Map<String, WidgetBuilder> getAplicationRoutes() {
         'avatar' : (BuildContext context ) => AvatarPage(),
         'card'   : (BuildContext context ) => CardPage(),
         'animatedContainer'   : (BuildContext context ) => AnimatedContainerPage(),
+        'inputs'   : (BuildContext context ) => InputPage(),
       };
       
 }
