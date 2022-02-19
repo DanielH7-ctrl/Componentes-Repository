343cffff152b3d4970728b13f04978a6d597d6a1
DanielH7-ctrl <danielhuerta826@gmail.com>
2022-02-18 19:46:12 -0500

    Rutas generadas dinamicamente

---
+++ lib/src/routes/routes.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import '../pages/alert_page.dart';
+import '../pages/avatar_page.dart';
+import '../pages/home_page.dart';
+
+Map<String, WidgetBuilder> getAplicationRoutes() {
+
+  return <String, WidgetBuilder>{
+        '/'      : (BuildContext context ) => HomePage(),
+        'alert'  : (BuildContext context ) => AlertPage(), 
+        'avatar' : (BuildContext context ) => AvatarPage(),
+      };
+      
+}
+
